e631581693b17c20397c4d127ecd1ecb3022770f
chinaPancake <sitletto@gmail.com>
2023-05-27 22:20:23 +0200

    new contract

--- web3py.py
+++ web3py.py
@@ -14,16 +14,19 @@ if w3.is_connected():
 else:
     print("Connection Failed")
 
-contract_address: str = "0xC700aA5AD1f437e42Dc690CA5711ec6bc47B3Fc3"
+contract_address: str = "0x5DDdBf0C22d24D0aB4A042afd7bA7e7a2e63e835"
 
 contract = w3.eth.contract(address=contract_address, abi=contract_abi)
-# name = contract.functions.name().call({'from': "0xD6D610866A888Ce99c0778EE518D7628c1A9a1e4"})
 name = contract.functions.name().call()
 
 def balanceOf_call(wallet:str):
     return contract.functions.balanceOf(wallet)
 
-def tokenOfOwnerByIndex_call(balanceof:int):
+def tokenOfOwnerByIndex_call(balanceof:int, wallet:str):
+    tokenOOBI_list = []
+    for i in range(0, balanceof):
+        tokenOOBI_list.append(contract.tokenOfOwnerByIndex(wallet, balanceof).call())
+        contract.tokenURI(tokenOOBI)
 
 
 balanceof = balanceOf_call('0x1d81532a666bb93a610d62b62cc264fb9Bc704Ed').call()

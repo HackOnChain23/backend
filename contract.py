6e31231ba00c4bf787e9d8ce57d86817643f7209
Piotr Szacilowski <szacilowski.piotr@gmail.com>
2023-05-27 13:36:10 +0200

    push contract interaction

--- contract.py
+++ contract.py
@@ -17,6 +17,7 @@ else:
 contract_address: str = "0xC700aA5AD1f437e42Dc690CA5711ec6bc47B3Fc3"
 
 contract = w3.eth.contract(address=contract_address, abi=contract_abi)
-name = contract.functions.name().call({'from': "0xD6D610866A888Ce99c0778EE518D7628c1A9a1e4"})
+# name = contract.functions.name().call({'from': "0xD6D610866A888Ce99c0778EE518D7628c1A9a1e4"})
+name = contract.functions.name().call()
 
 print(name)

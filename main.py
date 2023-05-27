5dae60cadfcd9b5860d24801b2e2a15a17fca04d
Piotr Szacilowski <szacilowski.piotr@gmail.com>
2023-05-27 19:45:22 +0200

    wip

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from PIL import Image
 import io
 
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile
 from loguru import logger as LOG
 
 from image_modifier import generate_composite_image_background_position
 from nft_storage_client import NFTStorageClient
-from invitations import generate_invitation_code
 
 app = FastAPI()
 
@@ -24,43 +24,99 @@ app.add_middleware(
 )
 
 
+class MetadataInput(BaseModel):
+    name: str
+    description: str
+    image: str
+    position: int
+    wallet_address: str
+    token_id: str
+    # nft_parts: List[str] = Field(..., alias='nftParts')
+
+
 @app.get("/healthcheck")
 def read_root():
     return {"status": "ok"}
 
 
-@app.post("/image/")
-def create_picture(wallet_address: str, first_art: UploadFile = File(...)):
-    LOG.info(f"New art was initiated by the wallet: {wallet_address}")
+@app.post("/image")
+def create_picture(first_art: UploadFile = File(...)):
+    LOG.info(f"New NFT was initiated")
 
-    template_image_path: str = "images/template.png"
-    template_image = Image.open(template_image_path)
-
-    image = Image.open(first_art.file)
-
-    composite_image = generate_composite_image_background_position(
-        template_image, image, 1
-    )
-
-    image_bytes = io.BytesIO()
-    composite_image.save(image_bytes, format="PNG")
-    image_bytes.seek(0)
-
-    response = client.upload_file(image_bytes)
+    response = client.upload_file(first_art.file)
 
     cid = response["value"]["cid"]
-
     url = NFTStorageClient.generate_nft_storage_url(cid=cid)
+
     LOG.info(f"NTF url: {url}")
 
-    # save to database (wallet_address, invitation_code, url)
-    # do we need wallet address? or only invitation code is needed?
-
     return {"url": url}
+
+# @app.post("/image/{position}")
+# def create_picture(position: int, first_art: UploadFile = File(...)):
+#     LOG.info(f"New art was initiated with position: {position}")
+#
+#     # upload to ipfs
+#     # dane od usera
+#     # z danych tworzymy jsona nazwa URI
+#     # upload jsona
+#     # zrwotn na front
+#
+#     template_image_path: str = "images/template.png"
+#     template_image = Image.open(template_image_path)
+#
+#     image = Image.open(first_art.file)
+#
+#     composite_image = generate_composite_image_background_position(
+#         template_image, image, position,
+#     )
+#
+#     image_bytes = io.BytesIO()
+#     composite_image.save(image_bytes, format="PNG")
+#     image_bytes.seek(0)
+#
+#     response = client.upload_file(image_bytes)
+#
+#     cid = response["value"]["cid"]
+#
+#     url = NFTStorageClient.generate_nft_storage_url(cid=cid)
+#     LOG.info(f"NTF url: {url}")
+#
+#     # save to database (wallet_address, invitation_code, url)
+#     # do we need wallet address? or only invitation code is needed?
+#
+#     return {"url": url}
+
+
+@app.post("/metadata")
+def add_metadata(metadata_input: MetadataInput):
+    LOG.info(f"Metadata creation")
+
+    metadata = client.upload_json(metadata=metadata_input)
+    # metadata_input.token_id: str TUTAJ ODPYTUJE smartcontract?
+    # token id jest po to do ktorego nfka mam dodac ta nowa czesc
+    # adress walleta -> smartcontract
+    # zwraca liste id dla danego usera
+    # meczuje tokenId od palucha z lista token idkow od lecha
+
+    #
+    # smart contract functions
+    # balanceOf(wallet_addres) -> ile lacznie mam nfki 3, 5
+    # tokenOfOwnerByIndex(address owner, uint256 index) - 3,5 razy mysze wykonac to mi zraca tokenId
+
+    LOG.info(f"Metadata: {metadata}")
+    return metadata
 
 
 @app.patch("/image/{position}")
 def update_picture(position: int, file: UploadFile = File(...)):
+    # get position from url
+
+    # input wallet address
+    # check wallet is owner of nft using smart contract
+    # get url of image from given url
+    # add image from
+
     LOG.info(f"Updating position {position}")
     return {"filename": file.filename}
 
@@ -68,3 +124,7 @@ def update_picture(position: int, file: UploadFile = File(...)):
 # @app.patch("/update-file/{position}")
 # async def update_picture(position: int, file: UploadFile = File(...)):
 #     return {"filename": file.filename}
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- nft_storage_client.py
+++ nft_storage_client.py
@@ -1,5 +1,7 @@
+import io
+import json
+
 import requests as r
-import pprint
 
 from loguru import logger as LOG
 
@@ -46,6 +48,33 @@ class NFTStorageClient:
             raise Exception(
                 f"Failed to retrieve file metadata with status code {resp.status_code}"
             )
+
+    def upload_json(self, metadata):
+        data = {
+            "name": metadata.name,
+            "description": metadata.description,
+            "image": metadata.image,  # TODO: add combined images
+            "nftParts": [
+                metadata.image,
+            ]
+        }
+
+        data_io = io.StringIO()
+        json.dump(data, data_io)
+        data_io.seek(0)
+
+        headers = {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGQzZTQ1ZTkyZmZiMzQ1RDdmZTQ2MzhkZDIyODZBM2U0ODNBODE2NjMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4NDc4MzE3OTcyOSwibmFtZSI6ImFydF9tZXJnZSJ9.67_OKDpOJ_qk_vskD_qyJJIr3xNowQzueCFWRg2BKBY',
+        }
+
+        resp = r.post("https://api.nft.storage/upload", headers=headers, data=data_io)
+
+        if resp.status_code == 200:
+            LOG.info("Metadata uploaded successfully")
+            return resp.json()
+        else:
+            LOG.exception(f"Metadata storage failed with status code {resp.status_code}")
+            raise Exception(f"Metadata storage failed with status code {resp.status_code}")
 
     def generate_nft_storage_url(cid: str):
         return cid + ".ipfs.nftstorage.link"

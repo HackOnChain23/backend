1d9fda9ab95a6452621d19b3f051032f78c31775
chinaPancake <sitletto@gmail.com>
2023-05-30 00:32:09 +0200

    moj

--- get_json.py
+++ get_json.py
@@ -12,19 +12,18 @@ w3 = Web3(Web3.HTTPProvider("https://rpc.ankr.com/mantle_testnet"))
 contract_address: str = CONTRACT_ADDRESS
 
 contract = w3.eth.contract(address=contract_address, abi=contract_abi)
-name = contract.functions.name().call()
 
 
 def balanceOf_call(wallet: str):
     tokenOOBI_list = []
     tokenURI_list = []
     lista_jsonow = []
+
     balance_amount = contract.functions.balanceOf(wallet).call()
 
     for i in range(0, balance_amount):
-        tokenOOBI_list.append(contract.functions.tokenOfOwnerByIndex(wallet, i))
-    for j in range(len(tokenOOBI_list)):
-        tokenURI_list.append(contract.functions.tokenURI(j).call())
+        s = tokenOOBI_list.append(contract.functions.tokenOfOwnerByIndex(wallet, i))
+        tokenURI_list.append(contract.functions.tokenURI(s).call())
 
     for k in range(len(tokenURI_list)):
         r = requests.get(tokenURI_list[k])
@@ -35,4 +34,4 @@ def balanceOf_call(wallet: str):
     return lista_jsonow
 
 
-# print(balanceOf_call("0x8caEa39280b90F74BB2251cbD46b85f7A500dd89"))
+print(balanceOf_call("0xAaee830dC9A872e7A907EA037d5ddbCA3BD621a2"))
